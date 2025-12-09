19ec19da8b18a7944dd12240efb2597966e4da32
juanlozanomendez6-svg <juanlozanomendez6@gmail.com>
2025-12-09 12:20:57 -0600

    Actualización de invetariohistorial modelo

--- src/models/venta.model.js
+++ src/models/venta.model.js
@@ -1,35 +1,39 @@
-import { DataTypes } from 'sequelize';
-import { sequelize } from '../config/db.js';
-import Usuario from './usuario.model.js';
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/db.js";
+import Usuario from "./usuario.model.js";
 
-const Venta = sequelize.define('Venta', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
+const Venta = sequelize.define(
+  "Venta",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    usuario_id: {
+      type: DataTypes.STRING, // 👈 MongoDB ID es string
+      allowNull: false,
+    },
+    total: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        min: 0,
+      },
+    },
+    fecha: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
   },
-  usuario_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  total: {
-    type: DataTypes.DECIMAL(10, 2),
-    allowNull: false,
-    validate: {
-      min: 0
-    }
-  },
-  fecha: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW
+  {
+    tableName: "ventas",
+    timestamps: false,
   }
-}, {
-  tableName: 'ventas',
-  timestamps: false
-});
+);
 
 // Relaciones
-Venta.belongsTo(Usuario, { foreignKey: 'usuario_id', as: 'usuario' });
-Usuario.hasMany(Venta, { foreignKey: 'usuario_id', as: 'ventas' });
+Venta.belongsTo(Usuario, { foreignKey: "usuario_id", as: "usuario" });
+Usuario.hasMany(Venta, { foreignKey: "usuario_id", as: "ventas" });
 
 export default Venta;
